1c0c087aaf4a9fea0f06ce0880dd3ea6667d9224
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-04 22:55:19 +0200

    adding a theme and constance of app

---
+++ lib/components/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const KBackgroundcolor = Color(0x009a9695);
+const KPrimarycolor = Color(0x00f0f0f0);
+const KTextcolor = Color(0x00070707);

d43db5c124757bfc23ec824ee22c691b5e535a35
Tota <71022738+Tota6@users.noreply.github.com>
2022-11-10 12:51:41 +0200

    using the defult values to the main and removing the manual identification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_ui_app/components/constants.dart';
+
+import 'Icon_and _details.dart';
+import 'buttons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        backgroundColor: KBackgroundcolor,
+        primaryColor: KBackgroundcolor,
       ),
       home: const MyHomePage(),
     );
@@ -30,11 +35,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 229, 229, 229),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: <Widget>[
+            const Buttons(),
+            const IconAndDetails(),
+          ],
         ),
       ),
     );
